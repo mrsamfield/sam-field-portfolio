7c65952a3b2e13c117a36cc4ea565f71f05acc3a
Glitch (glib-tortoiseshell-chronometer) <none>
2020-04-28 16:04:16 +0000

    😹🎀 Checkpoint ./animation.js:12520343/3519

--- animation.js
+++ animation.js
@@ -1,64 +1,59 @@
-const container = document.querySelector('section.bganimation')
+const container = document.querySelector("section.bganimation");
 
 const params = {
   width: `${container.getBoundingClientRect().width}`,
   height: `${container.getBoundingClientRect().height}`
-}
+};
 
-const two = new Two(params)
-two.appendTo(container)
+const two = new Two(params);
+two.appendTo(container);
 
 // config for our animation
-const numberOfShapes = 30
-const shapes = []
+const numberOfShapes = 30;
+const shapes = [];
 
-const lineWeight = params.height / (numberOfShapes * 2)
-const startWidth = params.width
-const loopDuration = 5 * 60
-const aDelay = 0.001
+const lineWeight = params.height / (numberOfShapes * 2);
+const startWidth = params.width;
+const loopDuration = 5 * 60;
+const aDelay = 0.001;
 
-const startRotation = 0
-const endRotation = fullRotation * 180 / 360  
-
+const startRotation = 0;
+const endRotation = (fullRotation * 180) / 360;
 
 // make shapes
 for (let i = 0; i < numberOfShapes; i++) {
-  const x = params.width * 0.5
-  const y = i * (lineWeight * 2) + lineWeight
+  const x = params.width * 0.5;
+  const y = i * (lineWeight * 2) + lineWeight;
 
-  const shape = two.makeRectangle(x, y, startWidth, lineWeight)
-  shape.noStroke()
-  shape.fill = 'rgba(255, 255, 255, 0.25)'
-  shapes.push(shape)
+  const shape = two.makeRectangle(x, y, startWidth, lineWeight);
+  shape.noStroke();
+  shape.fill = "rgba(255, 255, 255, 0.25)";
+  shapes.push(shape);
 }
 
-two.bind('update', function(frameCount) {
+two.bind("update", function(frameCount) {
   // main timeline
-  const currentFrame = frameCount % loopDuration
-  const t = currentFrame / loopDuration
-  
+  const currentFrame = frameCount % loopDuration;
+  const t = currentFrame / loopDuration;
 
   // 2 timelines
   shapes.forEach((shape, i) => {
-    let aStart = aDelay * i
-    let aEnd = aDelay * (numberOfShapes - i)
-    let r = startRotation
-    let w = startWidth
- 
-    
-      
-    if (t <= 0.5) {
-      const u = mapAndClamp(t, 0 + aStart, 0.5 - aEnd, 0, 1)
-      w = mapAndClamp(u, 0, 1, startWidth, lineWeight)
-      r = mapAndClamp(u, 0, 1, startRotation, endRotation)
-    } else {
-      const u = mapAndClamp(t, 0.5 + aStart, 1 - aEnd, 0, 1)
-      w = mapAndClamp(u, 0, 1, startWidth, lineWeight)
-      r = mapAndClamp(u, 0, 1, startRotation, endRotation)
-    }
-    shape.rotation = r
-    shape.width = w
-  })
-})
+    let aStart = aDelay * i;
+    let aEnd = aDelay * (numberOfShapes - i);
+    let r = startRotation;
+    let w = startWidth;
 
-two.play()
+    r = mapAndClamp(t, 0, 1, startRotation, endRotation);
+
+    if (t <= 0.5) {
+      w = mapAndClamp(t, 0, 0.5, startWidth, lineWeight);
+    } else {
+      w = mapAndClamp(t, 0.5, 1, lineWeight, startWidth);
+    }
+
+    shape.rotation = r;
+    shape.width = w;
+  });
+});
+
+two.play();
