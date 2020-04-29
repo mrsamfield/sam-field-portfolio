bd1c1fa2a37dd05c24dd690fba64d17fd5966caf
Glitch (glib-tortoiseshell-chronometer) <none>
2020-04-29 16:41:34 +0000

    🎀📖 Checkpoint ./style.css:12520343/54 ./cursor.js:12520343/293

---
+++ cursor.js
@@ -0,0 +1,6 @@
+const cursorContainer = document.querySelector("div.cursor-container")
+
+const createCursor = function () {
+    const cursor = cursorContainer.appendChild("div")
+    cursor.classList.add()
+}
