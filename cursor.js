b6874222d11ad6256f3682a4804026fc706cfaa5
Glitch (glib-tortoiseshell-chronometer) <none>
2020-04-29 17:27:49 +0000

    🌆🌓 Checkpoint ./index.html:12520343/5397 ./style.css:12520343/155 ./cursor.js:12520343/503

---
+++ cursor.js
@@ -0,0 +1,19 @@
+const cursorContainer = document.querySelector("div.cursor-container")
+let cursor = ""
+
+const createCursor = function () {
+    cursor = cursorContainer.appendChild("div")
+    cursor.classList.add("cursor")
+}
+
+const lockCursor = function (cursor, x, y) {
+    cursor.style.top = y
+    cursor.style.left = x 
+}
+
+createCursor()
+
+
+document.addEventListener("mousemove", function (e) {
+  lockCursor(cursor, e.clientX, e.clientY)
+})
