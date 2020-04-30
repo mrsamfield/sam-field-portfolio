c14af039b11355d025de4bc26e26b4d784a0f9eb
Glitch (glib-tortoiseshell-chronometer) <none>
2020-04-30 13:53:38 +0000

    🎲🍭 Checkpoint ./stickers.js:12520343/4762

--- stickers.js
+++ stickers.js
@@ -1,63 +1,58 @@
-const stickerSpace = document.querySelector('div.stickerSpace')
-let slide = 0
-let z = 1
-let stickers = []
-let moveable = ""
+const stickerSpace = document.querySelector("div.stickerSpace");
+let slide = 0;
+let z = 1;
+let stickers = [];
+let moveable = "";
 
 const labels = [
-  {copy: "Hi. I'm Sam"},
-  {copy: 'I am currently based in Liverpool'},
-  {copy: '(a very cool city)'},
-  {copy: 'I am a technical/ creative artworker'},
-  {copy: 'I love all things digital design'}
-]
+  { copy: "Hi. I'm Sam" },
+  { copy: "I am currently based in Liverpool" },
+  { copy: "(a very cool city)" },
+  { copy: "I am a technical/ creative artworker" },
+  { copy: "I love all things digital design" }
+];
 
-const stick = function (x, y) {
-  console.log('stick run')
-
-  z = z + 1
-  const rotation = Math.floor(Math.random() * 30) - 15
-  console.log('x = ', x, ' y = ', y, 'rotation = ', rotation)
-
+const stick = function(x, y) {
+  z = z + 1;
+  const rotation = Math.floor(Math.random() * 30) - 15;
   //Creates a new p element and inserts text
-  var sticker = document.createElement('div')
-  sticker.innerHTML = labels[slide].copy
-  sticker.classList.add('label')
-  sticker.style.left = `${x}px`
-  sticker.style.top = `${y}px`
-  sticker.style.transform = `rotate(${rotation}deg) translate(-50%, -50%)`
-  sticker.style.zIndex = z
+  var sticker = document.createElement("div");
+  sticker.innerHTML = labels[slide].copy;
+  sticker.classList.add("label");
+  sticker.style.left = `${x}px`;
+  sticker.style.top = `${y}px`;
+  sticker.style.transform = `rotate(${rotation}deg) translate(-50%, -50%)`;
+  sticker.style.zIndex = z;
   //appends text to stickerspace
-  document.getElementById('stickerSpace').appendChild(sticker)
-  slide = slide + 1
-  stickers.push(sticker)
-}
+  document.getElementById("stickerSpace").appendChild(sticker);
+  slide = slide + 1;
+  stickers.push(sticker);
+};
 
-const drag = function () {
-  
-}
-
-stickerSpace.addEventListener('click', function(e) {
-  console.log(e)
-  let target = e.target
-  if (e.target === stickerSpace) {
-  stick(e.clientX, e.clientY)
+const drag = function(x, y) {
+  if (moveable !== "") {
+    moveable.style.top = x;
+    moveable.style.left = y;
   }
-  for (let i = 0; i < stickers.length; i++){
-    if (e.target === stickers[i]) {
-      moveable = e.target
+};
+
+stickerSpace.addEventListener("mousedown", function(e) {
+  let target = e.target;
+  if (target === stickerSpace) {
+    stick(e.clientX, e.clientY);
+    moveable = ""
+  }
+  for (let i = 0; i < stickers.length; i++) {
+    if (target === stickers[i]) {
+      moveable = target;
     }
   }
-})
+});
 
-document.addEventListener("mouseup", function () {
-  moveable = ""
-})
+document.addEventListener("mouseup", function() {
+  moveable = "";
+});
 
-stickerSpace.addEventListener("mousemove", function (e) {
-  if (moveable === true) {
-  moveable.style.top = e.clientX
-  moveable.style.left = e.clientY
-  }
-})
-
+stickerSpace.addEventListener("mousemove", function(e) {
+  drag(e.clientX, e.clientY)
+});
